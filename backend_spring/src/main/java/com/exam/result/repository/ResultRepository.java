edb00fbdb67406535290fec9a60e32c63de1d02d
nethmini-11 <83303587+nethmini-11@users.noreply.github.com>
2022-09-24 11:35:50 +0530

    Implement result repository

--- backend_spring/src/main/java/com/exam/result/repository/ResultRepository.java
+++ backend_spring/src/main/java/com/exam/result/repository/ResultRepository.java
@@ -1,5 +1,15 @@
 package com.exam.result.repository;
 
-public class ResultRepository {
+import java.util.List;
 
+
+import org.springframework.data.mongodb.repository.MongoRepository;
+
+import com.exam.result.model.Result;
+
+
+public interface ResultRepository extends MongoRepository<Result, String> {
+  List<Result> findByPublished(boolean finalized);
+  List<Result> findByTitleContaining(String name);
 }
+
