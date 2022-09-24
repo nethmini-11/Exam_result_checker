df3cf3919a98c1dc440e93c44726a4d93c83d8bc
nethmini-11 <83303587+nethmini-11@users.noreply.github.com>
2022-09-24 10:52:16 +0530

    Create main class and model class

--- backend_spring/src/main/java/com/exam/result/BackendSpringApplication.java
+++ backend_spring/src/main/java/com/exam/result/BackendSpringApplication.java
@@ -1,4 +1,4 @@
-package com.example.demo;
+package com.exam.result;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
---
+++ backend_spring/src/main/java/com/exam/result/model/Result.java
@@ -0,0 +1,5 @@
+package com.exam.result.model;
+
+public class Result {
+
+}
